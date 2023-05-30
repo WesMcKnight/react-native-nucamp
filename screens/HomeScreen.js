caef0a2986743491c20290e5b78f3d4cbc4c7c00
Wes McKnight <wesleymcknight17@gmail.com>
2023-05-30 14:38:54 -0500

    components pt 2

---
+++ screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native/types";
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text>Home Screen</Text>
+        </View>
+    )
+}
+
+export default HomeScreen;
