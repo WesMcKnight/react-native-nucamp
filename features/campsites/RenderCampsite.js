7ec2dd6e8523f4b3cb6a6a0ed359a86fae4a313b
Wes McKnight <wesleymcknight17@gmail.com>
2023-06-16 06:00:37 -0500

    typo fix

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -77,6 +77,7 @@ const RenderCampsite = (props) => {
                         />
                         <Icon
                             name='pencil'
+                            type='font-awesome'
                             color='#5637DD'
                             raised
                             reverse
